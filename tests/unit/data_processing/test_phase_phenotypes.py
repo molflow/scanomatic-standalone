50c03cc0f02f76fc27c0fc9aa882138d5a94e247
Joakim Möller <joakim.moller@molflow.com>
2026-06-11 21:28:17 +0200

    test(data-processing): re-enable no-growth noise phenotype test

--- tests/unit/data_processing/test_phase_phenotypes.py
+++ tests/unit/data_processing/test_phase_phenotypes.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pytest
 
 from scanomatic.data_processing.phases.analysis import (
     DEFAULT_THRESHOLDS,
@@ -63,8 +62,8 @@ def build_model(phenotyper_object, test_curve):
         phenotyper_object, 0, (0, test_curve), DEFAULT_THRESHOLDS)
 
 
-@pytest.mark.skip(
-    reason='Fails randomly for the intercept though it should not')
+# @pytest.mark.skip(
+#     reason='Fails randomly for the intercept though it should not')
 def test_no_growth_only_noise():
 
     phenotyper_object = build_test_phenotyper()
